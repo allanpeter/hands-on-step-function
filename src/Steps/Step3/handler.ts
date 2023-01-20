b605da9cca98dcc9d9b44e5c9b039a52d37722f7
Allan Pimentel <allan.pimentel@seedz.ag>
2023-01-20 10:39:49 -0300

    first commit

---
+++ src/Steps/Step3/handler.ts
@@ -0,0 +1,4 @@
+export const handler = async (event: any) => {
+  // SALVA NO BANCO OU FAZ UM POST PRO MS
+  return event;
+}
---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+npm run offline '{"Token":"ZZZZ","Data":{"id":"1","document":"any-document"}}'
